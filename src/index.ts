e88b68a7f72891119b2b40c36cc63f239fc00585
Lee Seungyong <bongbongco@gmail.com>
2018-12-11 18:05:55 +0900

    Add GET KISA NOTICE & Editting Get KFISAC NOTICE

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
 import Cheerio from "cheerio";
 import Request from "request";
 
-const url = "https://www.krcert.or.kr/data/secNoticeList.do";
-Request(url, (error, response, html) => {
+
+const kisa_url = "https://www.krcert.or.kr/data/secNoticeList.do";
+Request(kisa_url, (error, response, html) => {
     if(error) {
         throw error;
     }
 
     const load_html = Cheerio.load(html);
-    console.log(load_html("#contentDiv > table > tbody > tr:nth-child > td.colTit > a").text());
+    let nth:any = null;
+
+    load_html("#contentDiv > table > tbody > tr > td.colTit > a").each((n) => {
+        nth = n + 1;
+        console.log(
+            load_html("#contentDiv > table > tbody > tr:nth-child("+ (<String>nth) +") > td.colTit > a"
+            ).text(),
+            load_html("#contentDiv > table > tbody > tr:nth-child("+ (<String>nth) +") > td.colTit > a"
+            ).attr("href"),
+            load_html("#contentDiv > table > tbody > tr:nth-child("+ (<String>nth) +") > td:nth-child(5)"
+            ).text()
+        );
+    });
 });
+
+const kfisac_url = "https://www.kfisac.or.kr/ContentsPage.doj"
+Request.post(kfisac_url, { headers: { "Cookie":""
+    }}, (error, response, html) => {
+        if(error) {
+            throw error;
+        }
+        
+        const load_html = Cheerio.load(html);
+        let nth:any = null;
+
+        load_html("#BdList-Type01 > tbody > tr > td.left > a").each((n) => {
+            nth = n + 1;
+            console.log(nth);
+            console.log(
+                load_html("#BdList-Type01 > tbody > tr:nth-child("+ (<String>nth) +") > td.left > a"
+                ).text()
+            );
+        });
+    }
+);
